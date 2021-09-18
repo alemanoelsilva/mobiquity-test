ab0f8aba4ed3ba5c74585362dd6643eebe4f909a
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2021-09-18 20:17:17 -0300

    feat: ensure Packer validate the filepath using the ReadFile

--- src/packer.ts
+++ src/packer.ts
@@ -1,5 +1,15 @@
+import { ReadFile } from './utils/file/read-file'
+
 export class Packer {
-  static pack(inputFile: string): string {
+  static async pack(inputFile: string): Promise<string> {
+    const readFile = new ReadFile(inputFile)
+
+    const isValidFile = await readFile.fileExists()
+
+    if (!isValidFile) {
+      throw new Error('File not found')
+    }
+
     return ''
   }
 }
