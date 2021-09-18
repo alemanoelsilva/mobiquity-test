2c63de6e5c0debaa071a7389750d18e32d8fefb7
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2021-09-18 20:17:38 -0300

    test: ensure Packer validate the filepath using the ReadFile

--- src/packer.spec.ts
+++ src/packer.spec.ts
@@ -1,9 +1,9 @@
 import { Packer } from './packer'
 
 describe('Packer Test', () => {
-  it('should return a string as result', () => {
-    const filepath = 'any filepath'
-    const result = Packer.pack(filepath)
+  it('should return a string when file is valid', async () => {
+    const filepath = 'resources/example_input'
+    const result = await Packer.pack(filepath)
 
     expect(result).toEqual('')
   })
