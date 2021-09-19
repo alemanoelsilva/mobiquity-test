d3207be9c3f733b3af2d7d8d047a8a3abd8b7fd6
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2021-09-19 17:59:27 -0300

    test: aggregate validations into PackageValidator

---
+++ src/utils/validator/package-validator.spec.ts
@@ -0,0 +1,54 @@
+import { PackageDto } from '../../domain/package-dto'
+import { PackageValidation } from './package-validation/package-validation'
+import { PackageValidator } from './package-validator'
+
+interface Types {
+  sut: PackageValidator
+  packageValidationStub: PackageValidation
+}
+
+const makePackageValidationStub = (): PackageValidation => {
+  class PackageValidationStub {
+    validateWeightLimit(): boolean {
+      return true
+    }
+
+    validteItemLimit(): boolean {
+      return true
+    }
+
+    validatePriceAndWeight(): boolean {
+      return true
+    }
+  }
+
+  return new PackageValidationStub()
+}
+
+const makeSut = (): Types => {
+  const packageValidationStub = makePackageValidationStub()
+
+  const sut = new PackageValidator(packageValidationStub)
+
+  return {
+    sut,
+    packageValidationStub
+  }
+}
+
+describe('Package Validator Test', () => {
+  it('should return true when packages are valid', () => {
+    const { sut } = makeSut()
+
+    const packages: PackageDto[] = [
+      {
+        packageLimitWeight: 100,
+        packagesOptions: [{ index: 1, weight: 50, price: 10 }]
+      }
+    ]
+
+    const result = sut.validate(packages)
+
+    expect(result).toBeTruthy()
+  })
+})
