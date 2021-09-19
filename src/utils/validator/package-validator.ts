c3e45fd84552762fe8aae07b9517b720e352d0d1
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2021-09-19 14:44:42 -0300

    feat: ensure PackageWeigthLimitValidator validates the items package limit

--- src/utils/validator/package-validator.ts
+++ src/utils/validator/package-validator.ts
@@ -1,10 +1,21 @@
 import { ApiError } from '../../errors/api-error'
-import { PackageWeightLimitValidator, PACKAGE } from './protocols/package-weight-limit-validator'
+import {
+  PackageWeightLimitValidator,
+  PackageItemLimitValidator,
+  PACKAGE
+} from './protocols/package-weight-limit-validator'
 
-export class PackageValidator implements PackageWeightLimitValidator {
+export class PackageValidator implements PackageWeightLimitValidator, PackageItemLimitValidator {
   validateWeightLimit(weight: number): boolean {
     if (weight >= PACKAGE.WEIGHT_LIMIT) {
       throw new ApiError(`The package weight (${weight}) is invalid, must be less than 100`)
+    }
+    return true
+  }
+
+  validteItemLimit(items: number): boolean {
+    if (items >= PACKAGE.ITEMS_LIMIT) {
+      throw new ApiError(`The package items (${items}) is invalid, must be less than 15`)
     }
     return true
   }
--- src/utils/validator/protocols/package-weight-limit-validator.ts
+++ src/utils/validator/protocols/package-weight-limit-validator.ts
@@ -1,7 +1,12 @@
 export enum PACKAGE {
-  WEIGHT_LIMIT = 100
+  WEIGHT_LIMIT = 100,
+  ITEMS_LIMIT = 15
 }
 
 export interface PackageWeightLimitValidator {
   validateWeightLimit: (weight: number) => boolean
+}
+
+export interface PackageItemLimitValidator {
+  validteItemLimit: (items: number) => boolean
 }
