094e482a1e0d7bf0cbf178bda1dcab86e2eea488
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2021-09-19 14:25:09 -0300

    test: ensure PackageWeigthLimitValidator throws the ApiError when weight is invalid

--- src/utils/validator/package-validator.spec.ts
+++ src/utils/validator/package-validator.spec.ts
@@ -9,5 +9,15 @@ describe('Package Validator Test', () => {
 
       expect(result).toBeTruthy()
     })
+
+    it('should throw an error when weight is higher than 100', () => {
+      const packageValidator = new PackageValidator()
+
+      try {
+        packageValidator.validateWeightLimit(101)
+      } catch (error) {
+        expect(error.message).toEqual('Error: The package weight (101) is invalid, must be less than 100')
+      }
+    })
   })
 })
