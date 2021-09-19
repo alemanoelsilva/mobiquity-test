e97328b3913ab677947b09a71e00998b2261e2a8
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2021-09-19 14:24:55 -0300

    feat: ensure PackageWeigthLimitValidator throws the ApiError when weight is invalid

--- src/utils/validator/package-validator.ts
+++ src/utils/validator/package-validator.ts
@@ -4,7 +4,7 @@ import { PackageWeigthLimitValidator, PACKAGE } from './protocols/package-weigth
 export class PackageValidator implements PackageWeigthLimitValidator {
   validateWeightLimit(weight: number): boolean {
     if (weight >= PACKAGE.WEIGTH_LIMIT) {
-      throw new ApiError(`The package weight is invalid, must be less than ${weight}`)
+      throw new ApiError(`The package weight (${weight}) is invalid, must be less than 100`)
     }
     return true
   }
