f9499af6d16539d36b0105a099102e75e95a3547
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2021-09-19 18:18:21 -0300

    feat: ensure Packer validates the file content calling the packageValidator

--- src/packer.ts
+++ src/packer.ts
@@ -1,22 +1,32 @@
 import { ApiError } from './errors/api-error'
 import { ManageFile } from './utils/file/manage-file'
 import { MapToPackage } from './utils/mappers/map-to-package'
+import { PackageValidation } from './utils/validator/package-validation/package-validation'
+import { PackageValidator } from './utils/validator/package-validator'
 
 export class Packer {
   static async pack(inputFile: string): Promise<string[]> {
-    const manageFile = new ManageFile(inputFile)
-    const mapToPackage = new MapToPackage()
+    try {
+      const manageFile = new ManageFile(inputFile)
+      const mapToPackage = new MapToPackage()
+      const packageValidator = new PackageValidator(new PackageValidation())
 
-    const isValidFile = await manageFile.existsFile()
+      const isValidFile = await manageFile.existsFile()
 
-    if (!isValidFile) {
-      throw new ApiError('File not found')
+      if (!isValidFile) {
+        throw new ApiError('File not found')
+      }
+
+      const fileContent = await manageFile.readFile()
+
+      const packages = fileContent.map(mapToPackage.mapPackage)
+
+      packageValidator.validate(packages)
+
+      return fileContent
+    } catch (error) {
+      // TODO implements logger
+      return error.message
     }
-
-    const fileContent = await manageFile.readFile()
-
-    fileContent.map(mapToPackage.mapPackage)
-
-    return fileContent
   }
 }
