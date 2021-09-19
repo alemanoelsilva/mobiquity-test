f5ef941acbe16eb850f72683671db7b2f3c9089c
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2021-09-18 21:01:23 -0300

    feat: ensure readFile returns an array of string with the file content splitted by '\n'

--- src/utils/file/manage-file.ts
+++ src/utils/file/manage-file.ts
@@ -2,14 +2,21 @@ import path from 'path'
 import fs from 'fs'
 import util from 'util'
 import { ExistFile } from './protocols/exists-file'
+import { ReadFile } from './protocols/read-file'
 
 const accessAsync = util.promisify(fs.access)
+const readFileAsync = util.promisify(fs.readFile)
 
-export class ManageFile implements ExistFile {
+export class ManageFile implements ExistFile, ReadFile {
   constructor(private readonly filepath: string) { }
 
+  private getPath(): string {
+    return path.join(path.resolve(), this.filepath)
+  }
+
   async existsFile(): Promise<boolean> {
-    const resolvedPath = path.join(path.resolve(), this.filepath)
+    const resolvedPath = this.getPath()
+
     try {
       await accessAsync(resolvedPath)
       return true
@@ -20,5 +27,13 @@ export class ManageFile implements ExistFile {
       }
       return false
     }
+  }
+
+  async readFile(): Promise<string[]> {
+    const resolvedPath = this.getPath()
+
+    const data: Buffer = await readFileAsync(resolvedPath)
+
+    return data.toString().split('\n')
   }
 }
---
+++ src/utils/file/protocols/read-file.ts
@@ -0,0 +1,3 @@
+export interface ReadFile {
+  readFile: () => Promise<string[]>
+}
