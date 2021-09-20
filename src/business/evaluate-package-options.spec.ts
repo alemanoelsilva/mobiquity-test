511172a9f8bd5b261e730d819e0f81d2e9056ae6
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2021-09-20 20:15:42 -0300

    refactor: ensure EvaluatePackageOption uses the new Possibilities.get method

--- src/business/evaluate-package-options.spec.ts
+++ src/business/evaluate-package-options.spec.ts
@@ -1,85 +1,63 @@
 import { PackageDto } from '../domain/models/package-dto'
+import { GeneratePossibilities } from '../domain/usecases/generate-possibilities'
 import { EvaluatePackageOption } from './evaluate-package-options'
 
+const makePossibilitiesStub = (): GeneratePossibilities => {
+  class Possibilities implements GeneratePossibilities {
+    private readonly possibilities: string[] = []
+
+    get(): string[] {
+      return this.possibilities
+    }
+
+    generate(): void {
+      this.possibilities.push('1')
+    }
+  }
+
+  return new Possibilities()
+}
+
+interface Types {
+  sut: EvaluatePackageOption
+  possibilitiesStub: GeneratePossibilities
+}
+
+const makeSut = (): Types => {
+  const possibilitiesStub = makePossibilitiesStub()
+
+  const sut = new EvaluatePackageOption(possibilitiesStub)
+
+  return {
+    sut,
+    possibilitiesStub
+  }
+}
+
 describe('Evaluate Package Options Test', () => {
-  it('should return a string with the index 4 as the best choice', () => {
-    const evaluatePackageOption = new EvaluatePackageOption()
+  it('should return a string with 1', () => {
+    const { sut } = makeSut()
 
     const packages: PackageDto = {
       packageLimitWeight: 81,
-      packagesOptions: [
-        { index: 1, weight: 53.38, price: 45 },
-        { index: 2, weight: 88.62, price: 98 },
-        { index: 3, weight: 78.48, price: 3 },
-        { index: 4, weight: 72.30, price: 76 },
-        { index: 5, weight: 30.18, price: 9 },
-        { index: 6, weight: 46.34, price: 48 }
-      ]
+      packagesOptions: [{ index: 1, weight: 53.38, price: 45 }]
     }
 
-    const result = evaluatePackageOption.getOptions(packages)
+    const result = sut.getOptions(packages)
 
-    expect(result).toEqual('4')
+    expect(result).toEqual('1')
   })
 
-  it('should return a string with "-" when there is no choice', () => {
-    const evaluatePackageOption = new EvaluatePackageOption()
+  it('should return a string with - when there is no valid package', () => {
+    const { sut } = makeSut()
 
     const packages: PackageDto = {
-      packageLimitWeight: 8,
-      packagesOptions: [
-        { index: 1, weight: 16.3, price: 34 }
-      ]
+      packageLimitWeight: 10,
+      packagesOptions: [{ index: 1, weight: 53.38, price: 45 }]
     }
 
-    const result = evaluatePackageOption.getOptions(packages)
+    const result = sut.getOptions(packages)
 
     expect(result).toEqual('-')
-  })
-
-  it('should return a string with the index 2 and 7 as the best choice', () => {
-    const evaluatePackageOption = new EvaluatePackageOption()
-
-    const packages: PackageDto = {
-      packageLimitWeight: 75,
-      packagesOptions: [
-        { index: 1, weight: 85.31, price: 29 },
-        { index: 2, weight: 14.55, price: 74 },
-        { index: 3, weight: 3.98, price: 16 },
-        { index: 4, weight: 26.24, price: 55 },
-        { index: 5, weight: 63.69, price: 52 },
-        { index: 6, weight: 76.25, price: 75 },
-        { index: 7, weight: 60.02, price: 74 },
-        { index: 8, weight: 93.18, price: 35 },
-        { index: 9, weight: 89.95, price: 78 }
-      ]
-    }
-
-    const result = evaluatePackageOption.getOptions(packages)
-
-    expect(result).toEqual('2,7')
-  })
-
-  it('should return a string with the index 8 and 9 as the best choice', () => {
-    const evaluatePackageOption = new EvaluatePackageOption()
-
-    const packages: PackageDto = {
-      packageLimitWeight: 56,
-      packagesOptions: [
-        { index: 1, weight: 90.72, price: 13 },
-        { index: 2, weight: 33.80, price: 40 },
-        { index: 3, weight: 43.15, price: 10 },
-        { index: 4, weight: 37.97, price: 16 },
-        { index: 5, weight: 46.81, price: 36 },
-        { index: 6, weight: 48.77, price: 79 },
-        { index: 7, weight: 81.80, price: 45 },
-        { index: 8, weight: 19.36, price: 79 },
-        { index: 9, weight: 6.76, price: 64 }
-      ]
-    }
-
-    const result = evaluatePackageOption.getOptions(packages)
-
-    expect(result).toEqual('8,9')
   })
 })
--- src/business/evaluate-package-options.ts
+++ src/business/evaluate-package-options.ts
@@ -1,23 +1,9 @@
 import { PackageDto, PackagesOptionDto } from '../domain/models/package-dto'
 import { EvaluateOptions } from '../domain/usecases/evaluate-options'
+import { GeneratePossibilities } from '../domain/usecases/generate-possibilities'
 
 export class EvaluatePackageOption implements EvaluateOptions {
-  private readonly possibilities: string[] = []
-
-  private getPossilitiesIds(ids: string, str: string): string {
-    if (!ids.length) return str
-
-    for (let i = 0; i < ids.length; i++) {
-      const ch = ids.charAt(i)
-      const left = ids.substring(0, i)
-      const right = ids.substring(i + 1)
-      const rest = left + right
-      const possibility = this.getPossilitiesIds(rest, str + ch)
-      this.possibilities.push(possibility)
-    }
-
-    return str
-  }
+  constructor(private readonly possibilities: GeneratePossibilities) { }
 
   private getWeightById(id: string, options: PackagesOptionDto[]): number {
     const option = options.find(option => option.index === Number(id))
@@ -33,7 +19,7 @@ export class EvaluatePackageOption implements EvaluateOptions {
     return 0
   }
 
-  getOptions(packages: PackageDto): any {
+  getOptions(packages: PackageDto): string {
     const {
       packageLimitWeight: limit,
       packagesOptions: options
@@ -47,22 +33,26 @@ export class EvaluatePackageOption implements EvaluateOptions {
 
     const ids = validOptions.reduce((acc, option) => `${acc}${option.index}`, '')
 
-    this.getPossilitiesIds(ids, '')
+    this.possibilities.generate(ids, '')
+    const possibilities = this.possibilities.get()
 
-    const possibility = new Map()
+    const possibilityMap = new Map()
 
-    this.possibilities
+    possibilities
       .sort()
       .forEach(id => {
         const ids = id.split('')
 
-        const weight = ids.reduce((acc, id) => (acc + this.getWeightById(id, validOptions)), 0)
-        const price = ids.reduce((acc, id) => (acc + this.getPriceById(id, validOptions)), 0)
+        const weight = ids
+          .reduce((acc: number, id: string) => (acc + this.getWeightById(id, validOptions)), 0)
 
-        if (weight <= limit) possibility.set(id, { weight, price })
+        const price = ids
+          .reduce((acc: number, id: string) => (acc + this.getPriceById(id, validOptions)), 0)
+
+        if (weight <= limit) possibilityMap.set(id, { weight, price })
       })
 
-    const [[bestChoice]] = [...possibility.entries()]
+    const [[bestChoice]] = [...possibilityMap.entries()]
       .filter(([key, { weight }]) => weight <= limit)
       .sort((possibilityA, possibilityB) => possibilityA[1].price < possibilityB[1].price ? 1 : -1)
 
