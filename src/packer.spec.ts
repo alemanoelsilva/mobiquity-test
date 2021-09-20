2f5204be67b12ffb6955301e40ed50ab47b6bb97
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2021-09-20 20:31:04 -0300

    test: ensure Packer calls the evaluatePackageOption to get the best package option

--- src/packer.spec.ts
+++ src/packer.spec.ts
@@ -35,4 +35,16 @@ describe('Packer Test', () => {
     const result = await Packer.pack(filepath)
     expect(result).toEqual('Error: The package price (150) is invalid, must be equal or less than 100')
   })
+
+  it('should return an array with the best choices according to input file', async () => {
+    const filepath = 'resources/example_input'
+
+    const result = await Packer.pack(filepath)
+
+    expect(result.length).toEqual(4)
+    expect(result[0]).toEqual('4')
+    expect(result[1]).toEqual('-')
+    expect(result[2]).toEqual('2,7')
+    expect(result[3]).toEqual('8,9')
+  })
 })
